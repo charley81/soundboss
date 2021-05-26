70aac4c305ae0764b8004ba271c87e530cd68609
ChrisKnowsDev <chrisharley81@gmail.com>
2021-05-26 09:54:30 -0400

    bellota-text font setup with fontsource package

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,11 @@
 import * as React from "react"
+import globalStyles from "../styles/global-styles"
+import { Global } from "@emotion/react"
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Global styles={globalStyles} />
       <header></header>
       <main>{children}</main>
       <footer></footer>
