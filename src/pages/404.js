9bc57ad51a832c4717ac0760c399dcfeee6ef788
ChrisKnowsDev <chrisharley81@gmail.com>
2021-06-07 19:58:30 -0400

    error page complete

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,29 @@
 import * as React from "react"
 import { Layout, Seo } from "../components"
+import { css } from "@emotion/react"
+import { Link } from "gatsby"
 
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <section
+      className="section"
+      css={css`
+        text-align: center;
+        margin-top: 10vh;
+
+        .btn {
+          margin-top: 2rem;
+        }
+      `}
+    >
+      {" "}
+      <h1>404: Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Link to="/" className="btn">
+        back home
+      </Link>
+    </section>
   </Layout>
 )
 
