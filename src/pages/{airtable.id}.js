677051a43e69ac59beaf0cd64f3be2890c424387
ChrisKnowsDev <chrisharley81@gmail.com>
2021-06-08 16:58:03 -0400

    programatic page setup complete

--- src/pages/{airtable.id}.js
+++ src/pages/{airtable.id}.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
-import { Layout, Title } from "../components"
+import { Layout, Title, Contact, Info } from "../components"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { css } from "@emotion/react"
 
@@ -17,15 +17,41 @@ const RoomInfoTemplate = ({ data }) => {
             border-radius: var(--border-radius);
           }
 
-          .info {
-            display: flex;
-            justify-content: space-between;
+          .room-info {
+            margin-bottom: 4rem;
+          }
 
+          .info {
             h4 {
               color: var(--color-primary);
               font-size: 2rem;
               font-weight: 300;
+              margin-top: 1rem;
             }
+
+            @media screen and (min-width: 768px) {
+              display: flex;
+              justify-content: space-between;
+
+              h4 {
+                margin-top: 0;
+              }
+            }
+          }
+
+          .equipment {
+            h4 {
+              font-size: 2rem;
+              font-weight: 300;
+            }
+
+            li {
+              margin: 1rem 0;
+            }
+          }
+
+          .equipment-items {
+            margin: 1rem 0;
           }
         `}
       >
@@ -48,9 +74,22 @@ const RoomInfoTemplate = ({ data }) => {
               <span className="name">{name}</span>
             </p>
           </div>
+
           {/* equipment */}
+          <div className="equipment">
+            <h4>Equipment:</h4>
+            <ul className="equipment-items">
+              {equipment.map((item, index) => {
+                return <li key={index}>- {item}</li>
+              })}
+            </ul>
+          </div>
+
           {/* contact component */}
+          <Contact />
+
           {/* info component */}
+          <Info />
         </section>
       </section>
     </Layout>
